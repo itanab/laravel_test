37ef7664d7ebf7363fc7cecd35903d92f3575f9b
itana <itanabulatovic.ib@gmail.com>
2019-11-07 14:29:01 +0100

    notifications

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -4,13 +4,22 @@ namespace App\Http\Controllers;
 use App\Mail\TestEmail;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Http\Request;
+use App\User;
+use App\Notifications\InvoicePaid;
 
 class EmailController extends Controller
 {
     public function index()
     {
+
+        //by email
         $user_name = 'Itana';
         Mail::to('mail@example.com')
             ->send(new TestEmail($user_name));
+
+        //using notification
+
+        $user = User::find(1);
+        $user->notify(new InvoicePaid());
     }
 }
---
+++ app/Notifications/InvoicePaid.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class InvoicePaid extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+            ->from('example@example.com', 'Example sender')
+            ->replyTo('example@example.com', 'Example sender')
+            ->subject('Invoice Paid')
+            ->greeting('Hello, '. $notifiable->name .'!')
+            ->line('One of your invoices has been paid!')
+            ->action('View Invoice', 'http://example.com')
+            ->line('Thank you for using our application!')
+            ->salutation('Notification Subject');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
